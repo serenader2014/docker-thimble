c05e6f52263fbacce8bdbf83846c10e10e8ddac4
serenader <xyslive@gmail.com>
2016-12-28 17:22:21 +0800

    update script

--- start.sh
+++ start.sh
@@ -19,8 +19,13 @@ echo $DB_URL
 echo $THIMBLE_DB_OAUTH
 echo $THIMBLE_DB_PUBLISH
 
-sed -i -e "s/postgres:\/\/\/publish/$DB_URL$THIMBLE_DB_PUBLISH/g" /var/thimble/publish.webmaker.org/.env
-cd /var/thimble/publish.webmaker.org/ && npm run knex
-su - postgres -c "psql $DB_URL$THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/create-tables.sql"
+sed -i -e "s/postgres\:\/\/\/publish/$DB_URL$THIMBLE_DB_PUBLISH/g" /var/thimble/publish.webmaker.org/.env
 sed -i -e "s/example.org\/oauth_redirect/localhost:3500\/callback/g" /var/thimble/id.webmaker.org/scripts/test-data.sql
-su - postgres -c "psql $DB_URL$THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/test-data.sql"
+cd /var/thimble/publish.webmaker.org/ && npm run knex
+if [ "$DB_URL" = 'postgresql://localhost/']; then
+    su - postgres -c "psql -d $THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/create-tables.sql"
+    su - postgres -c "psql -d $THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/test-data.sql"
+else
+    su - postgres -c "psql $DB_URL$THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/create-tables.sql"
+    su - postgres -c "psql $DB_URL$THIMBLE_DB_OAUTH -f /var/thimble/id.webmaker.org/scripts/test-data.sql"
+fi
